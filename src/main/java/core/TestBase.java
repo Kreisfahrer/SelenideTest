97056af2f1bff7e50ef8276febedc2c6bd2d7750
kreisfahrer <kreisfahrer@gmail.com>
2015-12-09 21:19:06 +0300

    Added a sample comment

--- src/main/java/core/TestBase.java
+++ src/main/java/core/TestBase.java
@@ -3,11 +3,14 @@ package core;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.io.*;
+import java.net.URL;
 import java.util.Properties;
 
 import static com.codeborne.selenide.WebDriverRunner.*;
@@ -18,6 +21,19 @@ public class TestBase {
 
     @BeforeTest
     public void configure() throws Exception {
+        String cloud = System.getProperty("browserStack", "");
+        if (cloud.length() > 0) {
+            String[] creds = cloud.split(" ");
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", System.getProperty("browser", "chrome"));
+            caps.setCapability("browser_version", System.getProperty("browserVersion", "42"));
+            caps.setCapability("os", System.getProperty("OS", "OS X"));
+            caps.setCapability("os_version", System.getProperty("OS_version", "Yosemite"));
+            String hubUrl = "http://" + creds[0] + ":" + creds[1] + "@hub.browserstack.com/wd/hub";
+            WebDriver driver = new RemoteWebDriver(new URL(hubUrl), caps);
+            WebDriverRunner.setWebDriver(driver);
+        }
+
         Configuration.timeout = 10000;
         Configuration.baseUrl = System.getProperty("baseUrl", DEFAULT_URL);
         //Configuration.browser = WebDriverRunner.HTMLUNIT;
