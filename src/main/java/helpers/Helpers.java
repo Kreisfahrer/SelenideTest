8fd1fd9afd98f9c803867969e7a0b0534ddde693
evgenmarchuk <evgenmarchuk@gmail.com>
2015-12-11 00:26:43 +0300

    Added static method getUrls with attr

--- src/main/java/helpers/Helpers.java
+++ src/main/java/helpers/Helpers.java
@@ -1,14 +1,26 @@
 package helpers;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Helpers {
     public static List<String> readAllLines(Path resourcePath) throws IOException {
         return Files.readAllLines(resourcePath, Charset.defaultCharset());
+    }
+
+    public static List<String> getUrls(ElementsCollection elements, String attribute) {
+        List<String> urls = new ArrayList<>();
+        for (SelenideElement url : elements) {
+            urls.add(url.attr(attribute));
+        }
+        return urls;
     }
 }
