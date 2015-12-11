58bda82d615e6e7453d282fbbb5b0ffe7db54ea8
saap_by <saap.net@gmail.com>
2015-12-11 03:05:41 +0300

    added new method getUrls

--- src/main/java/helpers/Helpers.java
+++ src/main/java/helpers/Helpers.java
@@ -1,14 +1,28 @@
 package helpers;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Helpers {
     public static List<String> readAllLines(Path resourcePath) throws IOException {
         return Files.readAllLines(resourcePath, Charset.defaultCharset());
     }
+
+    public static List<String> getUrls(ElementsCollection elements, String attribute) {
+        List<String> urls = new ArrayList<>();
+        for (SelenideElement image : elements) {
+            urls.add(image.attr(attribute));
+        }
+        return urls;
+    }
+
+
 }
