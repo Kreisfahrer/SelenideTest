466c8843e925029328321a8fc1b8d89e97c9ad2f
kreisfahrer <kreisfahrer@gmail.com>
2015-12-11 21:54:58 +0300

    Added screenshots after soft assert

--- src/main/java/helpers/listeners/CustomSoftAsserts.java
+++ src/main/java/helpers/listeners/CustomSoftAsserts.java
@@ -1,40 +1,12 @@
 package helpers.listeners;
 
 import com.codeborne.selenide.impl.SelenideLogger;
-import org.testng.ITestResult;
-import org.testng.reporters.ExitCodeListener;
+import com.codeborne.selenide.testng.SoftAsserts;
 
-/**
- * Created by Admin on 11.12.2015.
- */
-public class CustomSoftAsserts extends ExitCodeListener {
+public class CustomSoftAsserts extends SoftAsserts {
     private final CustomErrorsCollector errorsCollector = new CustomErrorsCollector();
 
     public CustomSoftAsserts() {
         SelenideLogger.addListener(this.errorsCollector);
-    }
-
-    public void onTestFailure(ITestResult result) {
-        super.onTestFailure(result);
-        this.failIfErrors(result);
-    }
-
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        super.onTestFailedButWithinSuccessPercentage(result);
-        this.failIfErrors(result);
-    }
-
-    public void onTestSuccess(ITestResult result) {
-        super.onTestSuccess(result);
-        this.failIfErrors(result);
-    }
-
-    public void onConfigurationFailure(ITestResult result) {
-        super.onConfigurationFailure(result);
-        this.failIfErrors(result);
-    }
-
-    private void failIfErrors(ITestResult result) {
-        this.errorsCollector.failIfErrors(result.getTestClass().getName() + '.' + result.getName());
     }
 }
