d80ce0497612ee18a4c9444a0cd8c7f3efe8a320
kreisfahrer <kreisfahrer@gmail.com>
2015-12-11 21:50:45 +0300

    Added screenshots after soft assert

---
+++ src/main/java/helpers/listeners/ScreenShooter.java
@@ -0,0 +1,14 @@
+package helpers.listeners;
+
+import helpers.Helpers;
+import org.testng.ITestResult;
+import org.testng.reporters.ExitCodeListener;
+
+public class ScreenShooter extends ExitCodeListener {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        super.onTestFailure(result);
+        Helpers.makeScreenshot();
+    }
+}
