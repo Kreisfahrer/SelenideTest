201853c150177c9073ed377d5896229ebcf06892
saap_by <saap.net@gmail.com>
2015-12-11 03:06:51 +0300

    Added new class and page

--- src/main/java/pages/StaticBrokenImagePage.java
+++ src/main/java/pages/StaticBrokenImagePage.java
@@ -1,5 +1,33 @@
 package pages;
 
-public class StaticBrokenImagePage {
+import org.openqa.selenium.By;
+import ru.yandex.qatools.allure.annotations.Step;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.$$;
+import static helpers.Helpers.getUrls;
+import static helpers.Locators.get;
+import static helpers.Locators.getLocatorString;
+
+public class StaticBrokenImagePage {
+    private final static By URLS_IMG = get("brokenImage.urlsImg");
+    private String attributeImg = "src";
+    private String pathLinkUrl = "broken_images";
+
+    @Step ("here built a list links")
+    public Map<String, String> imagesLoadedTest() {
+        List<String> urls = getUrls($$(URLS_IMG), attributeImg);
+        Map<String, String> brokenImages = new HashMap<>();
+        for (String link : urls) {
+
+        }
+        return brokenImages;
+    }
+
+    public String getPathLinkUrl() {
+        return pathLinkUrl;
+    }
 }
---
+++ src/test/java/BrokenImageBMPTest.java
@@ -0,0 +1,22 @@
+import net.lightbody.bmp.proxy.ProxyServer;
+import org.testng.annotations.BeforeMethod;
+import pages.FactoryRegistrationPage;
+import pages.StaticBrokenImagePage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+/**
+ * Created by saap_by on 11.12.2015.
+ */
+public class BrokenImageBMPTest {
+    private StaticBrokenImagePage staticBrokenImagePage;
+    private ProxyServer bmp;
+
+    @BeforeMethod
+    public void setup() {
+        staticBrokenImagePage = new StaticBrokenImagePage();
+        open(staticBrokenImagePage.getPathLinkUrl());
+    }
+
+
+}
