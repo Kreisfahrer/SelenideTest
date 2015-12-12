a61f53c28ca6f97154ff34d7adfde2b3b7ef7e43
saap_by <saap.net@gmail.com>
2015-12-12 23:48:44 +0300

    add method in helpers class add new test class

--- src/main/java/helpers/Helpers.java
+++ src/main/java/helpers/Helpers.java
@@ -1,5 +1,7 @@
 package helpers;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import ru.yandex.qatools.allure.annotations.Attachment;
@@ -9,7 +11,9 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
@@ -21,5 +25,21 @@ public class Helpers {
     @Attachment
     public static byte[] makeScreenshot() {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    public static String mapToString(Map<String, String> map) {
+        StringBuilder message = new StringBuilder();
+        for(String key : map.keySet()) {
+            message.append(String.format("url: %s, response code: %s\n", key, map.get(key)));
+        }
+        return message.toString();
+    }
+
+    public static List<String> getUrls(ElementsCollection elements, String attribute) {
+        List<String> urls = new ArrayList<>();
+        for (SelenideElement image : elements) {
+            urls.add(image.attr(attribute));
+        }
+        return urls;
     }
 }
---
+++ src/test/java/BrokenImageBMPTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by saap_by on 12.12.2015.
+ */
+public class BrokenImageBMPTest {
+
+}
