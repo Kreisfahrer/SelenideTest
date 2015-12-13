9d94519cf22d78353803a98ab3e7191be3f0b26d
evgenmarchuk <evgenmarchuk@gmail.com>
2015-12-14 00:16:35 +0300

    edited test method

--- src/test/java/StatusCodesTest.java
+++ src/test/java/StatusCodesTest.java
@@ -1,56 +1,43 @@
-import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.testng.SoftAsserts;
 import core.BmpTestBase;
-import net.lightbody.bmp.core.har.Har;
-import net.lightbody.bmp.core.har.HarEntry;
-import org.openqa.selenium.By;
-import org.testng.Assert;
+import net.lightbody.bmp.core.har.*;
+
 import org.testng.annotations.*;
 import pages.StatusCodesPage;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Configuration.AssertionMode.SOFT;
+import static com.codeborne.selenide.Configuration.AssertionMode.STRICT;
+import static org.testng.Assert.assertTrue;
 
+@Listeners(SoftAsserts.class)
 public class StatusCodesTest extends BmpTestBase{
 
     @BeforeMethod
     public void setup(){
         open("status_codes");
-
+        Configuration.assertionMode = SOFT;
+        Configuration.timeout = 0;
     }
 
     @Test
     public void responseCodePageTest() {
         int countLinks = $$(StatusCodesPage.CODE_PAGE_LINKS).size();
-        Map<String, String> responseCode = new HashMap<>();
         for (int i = 0; i < countLinks; i++) {
             server.newHar("the-internet");
-            $(getElement(StatusCodesPage.CODE_PAGE_LINKS, i)).click();
+            $(StatusCodesPage.CODE_PAGE_LINKS, i).click();
             Har har = server.getHar();
             for (HarEntry entry : har.getLog().getEntries()) {
-                if (entry.getResponse().getStatus() >= 400) {
-                    responseCode.put(entry.getRequest().getUrl(), String.valueOf(entry.getResponse().getStatus()));
-                }
+                assertTrue(entry.getResponse().getStatus() < 400);
             }
             back();
         }
-        Assert.assertEquals(responseCode.size(), 0, mapToString(responseCode));
     }
 
-    private SelenideElement getElement(By codePageLinks, int i) {
-        ElementsCollection links = $$(codePageLinks);
-        return links.get(i);
+    @AfterMethod
+    public void teardown() {
+        Configuration.assertionMode = STRICT;
     }
-
-    private String mapToString(Map<String, String> map) {
-        StringBuilder message = new StringBuilder();
-        for(String key : map.keySet()) {
-            message.append(String.format("\nurl: %s, response code: %s", key, map.get(key)));
-        }
-        message.append("\n");
-        return message.toString();
-    }
-
 }
