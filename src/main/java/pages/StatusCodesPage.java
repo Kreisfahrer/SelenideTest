88eb20ef4c5b5bc1ebff10cec6ad3e9d50bcccbf
evgenmarchuk <evgenmarchuk@gmail.com>
2015-12-13 16:34:10 +0300

    Added StatusCodesPage with locator

---
+++ src/main/java/pages/StatusCodesPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+import static helpers.Locators.get;
+
+public class StatusCodesPage {
+    public final static By CODE_PAGE_LINKS = get("statusCodesPage.codeLinks");
+}
