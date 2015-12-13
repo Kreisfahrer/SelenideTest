b5d145dbd2649546b2d2530a57e27005d4b7ecd6
saap_by <saap.net@gmail.com>
2015-12-13 04:28:36 +0300

    changed test class v1

--- src/test/java/BrokenImageBMPTest.java
+++ src/test/java/BrokenImageBMPTest.java
@@ -9,7 +9,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
+import static helpers.Helpers.getUrls;
 import static helpers.Helpers.mapToString;
 
 /**
@@ -17,6 +19,7 @@ import static helpers.Helpers.mapToString;
  */
 public class BrokenImageBMPTest extends BmpTestBase {
     private Har har;
+    private String url;
 
     @BeforeMethod
     public void setup() {
@@ -27,11 +30,17 @@ public class BrokenImageBMPTest extends BmpTestBase {
     @Test
     public void brokenImageTest() {
         har = server.getHar();
+        List<String> links = getUrls($$(".example>img"), "src");
         Map<String, String> brokenImage = new HashMap<>();
         for (HarEntry entry : har.getLog().getEntries()) {
+            url = entry.getRequest().getUrl();
             int responseCode = entry.getResponse().getStatus();
-            if (responseCode >= 400) {
-                brokenImage.put(entry.getRequest().getUrl(), String.valueOf(responseCode));
+            for(String link : links) {
+                if (url.equals(link)) {
+                    if (responseCode >= 400) {
+                        brokenImage.put(entry.getRequest().getUrl(), String.valueOf(responseCode));
+                    }
+                }
             }
         }
         Assert.assertEquals(brokenImage.size(), 0, mapToString(brokenImage));
