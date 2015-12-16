dac11da2bc315e3334034c2f2a23a86b841062d8
kreisfahrer <kreisfahrer@gmail.com>
2015-12-16 18:58:35 +0300

    After review commit

--- src/test/java/AuthenticationFormTest.java
+++ src/test/java/AuthenticationFormTest.java
@@ -20,13 +20,10 @@ public class AuthenticationFormTest extends TestBase {
     @Test(dataProvider = "loginData", dataProviderClass = DataProviders.class)
     public void authenticationTest(String login, String pswd, String errorMessage) {
         StaticRegistrationPage.login(login, pswd);
-               if (!errorMessage.isEmpty()) {
-                                $(StaticRegistrationPage.FLASH_ERROR).should(appear, text(errorMessage));
-                    } else {
-                   $(AuthenticatedPage.FLASH_SUCCESS).should(appear,text("You logged into a secure area!"));
-                   AuthenticatedPage.logOut();
+        if (!errorMessage.isEmpty()) {
+            $(StaticRegistrationPage.FLASH_ERROR).should(appear, text(errorMessage));
+        } else {
+            $(AuthenticatedPage.FLASH_SUCCESS).should(appear,text("You logged into a secure area!"));
         }
-
     }
-
 }
