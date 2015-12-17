fea392f0a2981b72006982972253d2ff542c1ed0
evgenymarchuk <evgenmarchuk@gmail.com>
2015-12-17 15:20:09 +0300

    Edited testNg annotation

--- src/main/java/core/BmpTestBase.java
+++ src/main/java/core/BmpTestBase.java
@@ -3,7 +3,9 @@ package core;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import net.lightbody.bmp.proxy.ProxyServer;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 
@@ -13,7 +15,7 @@ public class BmpTestBase extends TestBase{
     protected ProxyServer server;
 
     @Override
-    @BeforeTest
+    @BeforeMethod
     public void configure() {
         Configuration.timeout = 10000;
         Configuration.baseUrl = System.getProperty("baseUrl", DEFAULT_URL);
@@ -38,6 +40,7 @@ public class BmpTestBase extends TestBase{
         WebDriverRunner.setProxy(null);
     }
 
+    @Override
     @AfterTest
     public void cleanup() {
         saveEnvironment();
